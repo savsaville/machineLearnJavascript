25ee0b20df75a3a6f0e7ae68fa919d6fa7e88dde
Richard Saville <richardasaville@gmail.com>
2017-07-03 15:55:15 +1000

    turn String values to floats, turn type into numbered classes

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ csv({ noheader: true, headers: names })
 
 /**
  * Randomize array element order in-place.
+ * https://stackoverflow.com/a/12646864
  * Using Durstenfeld shuffle algorithm.
  */
 
@@ -46,4 +47,46 @@ function shuffleArray(array) {
         array[j] = temp;
     }
     return array;
+}
+
+function dressData() {
+    /**
+     * There are three different types of Iris flowers
+     * that this dataset classifies.
+     *
+     * 1. Iris Setosa 
+     * 2. Iris Versicolor 
+     * 3. Iris Virginica 
+     *
+     * We are going to change these classes from Strings to numbers.
+     * Such that, a value of type equal to
+     * 0 would mean setosa,
+     * 1 would mean versicolor, and
+     * 3 would mean virginica
+     */
+
+    let types = new Set(); //To gather Unique classes
+
+    data.forEach((row) => {
+        types.add(row.type);
+    });
+
+    typesArray = [...types]; // To save the different types of classes
+
+    data.forEach((row) => {
+        let rowArray, typeNumber;
+
+        rowArray = Object.keys(row).map(key => parseFloat(row[key])).slice(0, 4);
+        typeNumber = typesArray.indexOf(row.type); //convert type(string) to type(number)
+
+        X.push(rowArray);
+        y.push(typeNumber);
+    });
+
+    trainingSetX = X.slice(0, seperationSize);
+    trainingSetY = y.slice(0, seperationSize);
+    testSetX = X.slice(seperationSize);
+    testSetY = y.slice(seperationSize);
+
+    train();
 }
