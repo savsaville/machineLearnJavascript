89337bd54f1017c5c5a29fd1ba655a24aa7e0ece
Richard Saville <richardasaville@gmail.com>
2017-07-03 15:38:12 +1000

    shuffle algorithm to shuffle dataset

--- index.js
+++ index.js
@@ -24,3 +24,26 @@ let trainingSetX = [],
 
 csv({ noheader: true, headers: names })
     .fromFile(csvFilePath)
+    .on('json', (jsonObj) => {
+        data.push(jsonObj); //push each object to data Array
+    })
+    .on('done', (error) => {
+        seperationSize = 0.8 * data.length;
+        data = shuffleArray(data);
+        dressData();
+    });
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
